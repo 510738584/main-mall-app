05d6014850d6bd79bf043e6e8e762a3d17e302a7
510738584 <510738584@qq.com>
2021-06-28 10:35:33 +0800

    整理layout和使用vueX

---
+++ src/layout/components/sliderNav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main-header">
+    <a-button
+      type="primary"
+      style="margin-bottom: 16px"
+      @click="toggleCollapsed"
+    >
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <div class="breadcrums">
+      <a-breadcrumb>
+        <a-breadcrumb-item>商品</a-breadcrumb-item>
+        <a-breadcrumb-item><a href="">商品统计</a></a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <ul class="user-info">
+      <li>欢迎，dongmeipi<a-icon type="down" /></li>
+      <li>退出登录</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+@import "~@/assets/menu.less";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,20 @@ import Home from '@/layout/Home.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+const routes = [{
+  path: '/',
+  name: 'Home',
+  component: Home,
+  chlidren: [],
+},
+{
+  path: '/about',
+  name: 'About',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+},
 ];
 
 const router = new VueRouter({
